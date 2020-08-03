71afac56608062c4c2fe36072528dbdf62679c78
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2020-08-03 15:40:59 +0530

    setup api and variables in app

--- App.js
+++ App.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ScrollView, Text } from 'react-native';
+
+import fetchData from './src/api/fetchData';
 
 import Header from './src/components/Header';
 
 const App = () => {
+  
+  const [ city, setCity ] = useState('');
+  const [ cityData, setCityData ] = useState({});
+  const [ displayData, setDisplayData ] = useState(false);
+  const [ theme, setTheme ] = useState('');
+  let [ searchResults, setSearchResults ] = useState([]);
+  
+  const getData = async (city='delhi') => {
+      const data = await fetchData(city);
+      setCityData(data);
+      setDisplayData(true);
+      setSearchResults([]);
+      setCity('');
+  }
+
+  useEffect(() => {
+      if(  city.length > 2 ) {
+          let newData = searchData(city);
+          setSearchResults(newData);
+      } else {
+          setSearchResults([]); 
+      }
+  }, [city])
+
+  const clearData = () => {
+      setCity('');
+      setDisplayData(false);
+  }
+
   return (
     <ScrollView>
       <Header title='Get Weather'/>
